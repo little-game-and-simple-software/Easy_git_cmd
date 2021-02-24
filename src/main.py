9c0b96eb95254dd70a64044cbd096aa83daf389d
2439905184 <suchenglong018@126.com>
2021-02-24 13:34:26 +0800

    init

---
+++ src/main.py
@@ -0,0 +1,21 @@
+
+#添加一个读取ini配置文件的开源库
+#开发中
+def init():
+    #alt shift td打开命令行界面
+    #config_file=open("")
+    pass
+
+def welcome():
+    print("欢迎使用")
+    print("这个一个易于使用的git命令行。")
+    print("（懒得记代码所以做这个）。而且万一打错代码就挺麻烦的")
+    print("1.git add <添加>")
+    pass
+def process():
+    usr_input=input()
+
+    pass
+init()
+welcome()
+process()
